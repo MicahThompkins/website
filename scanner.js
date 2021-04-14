fd85486988ffbf5e9ec96a685ee719ff813db454
MICAH THOMPKINS <micahthompkins1@gmail.com>
2021-04-14 08:34:54 -0500

    got buttons to work as own react component, working on methodslist

--- scanner.js
+++ scanner.js
@@ -14,137 +14,278 @@ var ScannerApp = function (_React$Component) {
     function ScannerApp(props) {
         _classCallCheck(this, ScannerApp);
 
+        // this.handleCLick = this.handleClick.bind(this);
         var _this = _possibleConstructorReturn(this, (ScannerApp.__proto__ || Object.getPrototypeOf(ScannerApp)).call(this, props));
 
-        _this.handleCLick = _this.handleClick.bind(_this);
         _this.setDomain = _this.setDomain.bind(_this);
-        _this.processJson = _this.processJson.bind(_this);
+        // this.processJson = this.processJson.bind(this);
         _this.state = {
-            domain: ""
+            domain: "", method: ""
         };
         return _this;
     }
+    // isValidUrl(inputUrl){
+    //     if(inputUrl.includes(".")) {
+    //         try {
+    //             const url = new URL(inputUrl)
+    //             if (url.protocol === "http:" || url.protocol === "https:") {
+    //                 return url.hostname;
+    //             } else{
+    //                 return false;
+    //             }
+    //         } catch (TypeError) {
+    //             return false;
+    //         }
+    //     } else{
+    //         return false;
+    //     }
+    // }
+    // checkDomain(){
+    //     //TODO add more in depth error checking
+    //     let currDomain = this.state.domain
+    //     if (currDomain === ""){
+    //         return false;
+    //     } else if( (currDomain.substring(0,7) === "http://") || currDomain.substring(0,8) === "https://" ){
+    //         return this.isValidUrl(currDomain);
+    //     } else {
+    //         currDomain = "http://" + currDomain;
+    //         return this.isValidUrl(currDomain);
+    //     }
+    // }
+    // handleClick(button){
+    //     console.log(button);
+    //     let domainToSend = this.checkDomain();
+    //     if (domainToSend) {
+    //         const requestData = {
+    //             method: 'POST',
+    //             headers: {'Content-Type': 'application/json'},
+    //             body: JSON.stringify({"domain": this.state.domain, "method": button})
+    //
+    //         }
+    //         // const response = await fetch()
+    //         console.log("before Fetch")
+    //         fetch('http://127.0.0.1:5000/', requestData)
+    //             // .then(response =>response.json())
+    //             .then(response => response.json())
+    //             .then(json => this.processJson(json, button))
+    //         console.log("afte fetch")
+    //     } else {
+    //         alert("invalid url, please enter a valid url");
+    //     }
+    //         // console.log(response)
+    // }
+    //
+    // processJson(json, button){
+    //     console.log("json.output type: " + typeof json.output);
+    //     // if(json.output){
+    //     //     console.log("if json output is true");
+    //     // }
+    //     if (json.output === null || json.output === {}){
+    //         alert("invalid url, please enter a valid url");
+    //     } else{
+    //         alert(button + ": " + json.output);
+    //     }
+    // }
+
 
     _createClass(ScannerApp, [{
-        key: 'handleClick',
-        value: function handleClick(button) {
+        key: "setDomain",
+        value: function setDomain(event) {
             var _this2 = this;
 
-            console.log(button);
-            var requestData = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ "domain": this.state.domain, "method": button })
-                // const response = await fetch()
-            };console.log("before Fetch");
-            fetch('http://127.0.0.1:5000/', requestData)
-            // .then(response =>response.json())
-            .then(function (response) {
-                return response.json();
-            }).then(function (json) {
-                return _this2.processJson(json);
+            this.setState({ domain: event.target.value }, function () {
+                return _this2.render();
             });
-            console.log("afte fetch");
-            // console.log(response)
         }
     }, {
-        key: 'processJson',
-        value: function processJson(json) {
-            console.log(json);
-        }
-    }, {
-        key: 'setDomain',
-        value: function setDomain(event) {
-            this.setState({ domain: event.target.value });
-        }
-    }, {
-        key: 'render',
+        key: "render",
         value: function render() {
-            var _this3 = this;
-
             return React.createElement(
-                'div',
-                { className: 'ScannerApp' },
-                React.createElement(
-                    'div',
-                    { className: 'scanner_buttons' },
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('ipv4_addresses');
-                            } },
-                        'IPv4'
-                    ),
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('ipv6_addresses');
-                            } },
-                        'IPv6'
-                    ),
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('rdns_names');
-                            } },
-                        'RDNS'
-                    ),
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('rtt_range');
-                            } },
-                        'Round Trip Range'
-                    ),
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('tls_versions');
-                            } },
-                        'TLS'
-                    ),
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('http_server');
-                            } },
-                        'HTTP Server Name'
-                    ),
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('insecure_http');
-                            } },
-                        'Responds to HTTP'
-                    ),
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('redirect_to_https');
-                            } },
-                        'Redirects to HTTPS'
-                    ),
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('hsts');
-                            } },
-                        'HSTS'
-                    ),
-                    React.createElement(
-                        'button',
-                        { className: 'scanner_buttons', onClick: function onClick() {
-                                return _this3.handleClick('root_ca');
-                            } },
-                        'Root Certificate Authority'
-                    )
-                ),
-                React.createElement('input', { type: 'text', onChange: this.setDomain })
+                "div",
+                { className: "ScannerApp" },
+                React.createElement("input", { type: "text", value: this.state.domain, onChange: this.setDomain }),
+                React.createElement(MethodsList, { domain: this.state.domain })
             );
         }
     }]);
 
     return ScannerApp;
+}(React.Component);
+// function MethodsButtons(props){
+
+
+var MethodsList = function (_React$Component2) {
+    _inherits(MethodsList, _React$Component2);
+
+    function MethodsList(props) {
+        _classCallCheck(this, MethodsList);
+
+        var _this3 = _possibleConstructorReturn(this, (MethodsList.__proto__ || Object.getPrototypeOf(MethodsList)).call(this, props));
+
+        console.log("In methods buttons props.domain: " + props.domain);
+        return _this3;
+    }
+
+    _createClass(MethodsList, [{
+        key: "render",
+        value: function render() {
+            console.log("in methods buttons render props.domain");
+            return React.createElement(
+                "div",
+                { className: "scanner_methods_list" },
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "ipv4_addresses" }),
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "ipv6_addresses" }),
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "rdns_names" }),
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "rtt_range" }),
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "tls_versions" }),
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "http_server" }),
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "insecure_http" }),
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "redirect_to_https" }),
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "hsts" }),
+                React.createElement(MethodsListItem, { domain: this.props.domain, method: "root_ca" })
+            );
+        }
+    }]);
+
+    return MethodsList;
+}(React.Component);
+
+var MethodsListItem = function (_React$Component3) {
+    _inherits(MethodsListItem, _React$Component3);
+
+    function MethodsListItem(props) {
+        _classCallCheck(this, MethodsListItem);
+
+        var _this4 = _possibleConstructorReturn(this, (MethodsListItem.__proto__ || Object.getPrototypeOf(MethodsListItem)).call(this, props));
+
+        console.log("In methods buttons props.domain: " + props.domain);
+        return _this4;
+    }
+
+    _createClass(MethodsListItem, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "scanner_method_list_item" },
+                React.createElement(MethodsButton, { domain: this.props.domain, method: this.props.method })
+            );
+        }
+    }]);
+
+    return MethodsListItem;
+}(React.Component);
+
+var MethodsButton = function (_React$Component4) {
+    _inherits(MethodsButton, _React$Component4);
+
+    function MethodsButton(props) {
+        _classCallCheck(this, MethodsButton);
+
+        var _this5 = _possibleConstructorReturn(this, (MethodsButton.__proto__ || Object.getPrototypeOf(MethodsButton)).call(this, props));
+
+        _this5.handleCLick = _this5.handleClick.bind(_this5);
+        _this5.processJson = _this5.processJson.bind(_this5);
+        // this.state ={
+        //     domain: this.props['domain'],
+        //     method: this.props['method']
+        // }
+        return _this5;
+    }
+
+    _createClass(MethodsButton, [{
+        key: "isValidUrl",
+        value: function isValidUrl(inputUrl) {
+
+            if (inputUrl.includes(".")) {
+                try {
+                    var url = new URL(inputUrl);
+                    if (url.protocol === "http:" || url.protocol === "https:") {
+                        return url.hostname;
+                    } else {
+                        return false;
+                    }
+                } catch (TypeError) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }, {
+        key: "checkDomain",
+        value: function checkDomain() {
+            //TODO add more in depth error checking
+
+            var currDomain = this.props.domain;
+            console.log(this.props.domain);
+            if (currDomain === "") {
+                return false;
+            } else if (currDomain.substring(0, 7) === "http://" || currDomain.substring(0, 8) === "https://") {
+                return this.isValidUrl(currDomain);
+            } else {
+                currDomain = "http://" + currDomain;
+                return this.isValidUrl(currDomain);
+            }
+        }
+    }, {
+        key: "handleClick",
+        value: function handleClick(method) {
+            var _this6 = this;
+
+            console.log(method);
+            var domainToSend = this.checkDomain();
+            if (domainToSend) {
+                console.log("this.props.domain: " + this.props.domain + " method: " + this.props.method);
+                var requestData = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ "domain": this.props.domain, "method": method })
+                    // const response = await fetch()
+                };console.log("before Fetch");
+                fetch('http://127.0.0.1:5000/', requestData)
+                // .then(response =>response.json())
+                .then(function (response) {
+                    return response.json();
+                }).then(function (json) {
+                    return _this6.processJson(json, method);
+                });
+                console.log("afte fetch");
+            } else {
+                alert("invalid url, please enter a valid url");
+            }
+            // console.log(response)
+        }
+    }, {
+        key: "processJson",
+        value: function processJson(json, method) {
+            if (json.output === null) {
+                alert("invalid url, please enter a valid url");
+            } else {
+                alert(method + ": " + json.output);
+            }
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            var _this7 = this;
+
+            // let method = this.props.method;
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "button",
+                    { className: "scanner_button", onClick: function onClick() {
+                            return _this7.handleClick(_this7.props.method);
+                        } },
+                    this.props.method
+                )
+            );
+        }
+    }]);
+
+    return MethodsButton;
 }(React.Component);
 
 ReactDOM.render(React.createElement(ScannerApp, null), rootElement);
--- src/scanner.js
+++ src/scanner.js
@@ -2,57 +2,225 @@ const rootElement = document.getElementById('scanner_react_container');
 class ScannerApp extends React.Component{
     constructor(props) {
         super(props);
-        this.handleCLick = this.handleClick.bind(this);
+        // this.handleCLick = this.handleClick.bind(this);
         this.setDomain = this.setDomain.bind(this);
-        this.processJson = this.processJson.bind(this);
+        // this.processJson = this.processJson.bind(this);
         this.state ={
-            domain: ""
+            domain: "", method: ""
         }
     }
-    handleClick(button){
-        console.log(button);
-        const requestData ={
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({"domain": this.state.domain, "method":button})
-
-        }
-        // const response = await fetch()
-        console.log("before Fetch")
-        fetch('http://127.0.0.1:5000/', requestData)
-            // .then(response =>response.json())
-            .then(response => response.json())
-            .then(json => this.processJson(json))
-        console.log("afte fetch")
-        // console.log(response)
-    }
-    processJson(json){
-        console.log(json)
-    }
+    // isValidUrl(inputUrl){
+    //     if(inputUrl.includes(".")) {
+    //         try {
+    //             const url = new URL(inputUrl)
+    //             if (url.protocol === "http:" || url.protocol === "https:") {
+    //                 return url.hostname;
+    //             } else{
+    //                 return false;
+    //             }
+    //         } catch (TypeError) {
+    //             return false;
+    //         }
+    //     } else{
+    //         return false;
+    //     }
+    // }
+    // checkDomain(){
+    //     //TODO add more in depth error checking
+    //     let currDomain = this.state.domain
+    //     if (currDomain === ""){
+    //         return false;
+    //     } else if( (currDomain.substring(0,7) === "http://") || currDomain.substring(0,8) === "https://" ){
+    //         return this.isValidUrl(currDomain);
+    //     } else {
+    //         currDomain = "http://" + currDomain;
+    //         return this.isValidUrl(currDomain);
+    //     }
+    // }
+    // handleClick(button){
+    //     console.log(button);
+    //     let domainToSend = this.checkDomain();
+    //     if (domainToSend) {
+    //         const requestData = {
+    //             method: 'POST',
+    //             headers: {'Content-Type': 'application/json'},
+    //             body: JSON.stringify({"domain": this.state.domain, "method": button})
+    //
+    //         }
+    //         // const response = await fetch()
+    //         console.log("before Fetch")
+    //         fetch('http://127.0.0.1:5000/', requestData)
+    //             // .then(response =>response.json())
+    //             .then(response => response.json())
+    //             .then(json => this.processJson(json, button))
+    //         console.log("afte fetch")
+    //     } else {
+    //         alert("invalid url, please enter a valid url");
+    //     }
+    //         // console.log(response)
+    // }
+    //
+    // processJson(json, button){
+    //     console.log("json.output type: " + typeof json.output);
+    //     // if(json.output){
+    //     //     console.log("if json output is true");
+    //     // }
+    //     if (json.output === null || json.output === {}){
+    //         alert("invalid url, please enter a valid url");
+    //     } else{
+    //         alert(button + ": " + json.output);
+    //     }
+    // }
     setDomain(event){
-        this.setState({domain: event.target.value})
+        this.setState({domain: event.target.value}, () => this.render());
     }
 
     render(){
         return(
             <div className ="ScannerApp">
-                <div className="scanner_buttons">
-                    <button className="scanner_buttons" onClick={() => this.handleClick('ipv4_addresses')}>IPv4</button>
-                    <button className="scanner_buttons" onClick={() => this.handleClick('ipv6_addresses')}>IPv6</button>
-                    <button className="scanner_buttons" onClick={() => this.handleClick('rdns_names')}>RDNS</button>
-                    <button className="scanner_buttons" onClick={() => this.handleClick('rtt_range')}>Round Trip Range</button>
-                    <button className="scanner_buttons" onClick={() => this.handleClick('tls_versions')}>TLS</button>
-                    <button className="scanner_buttons" onClick={() => this.handleClick('http_server')}>HTTP Server Name</button>
-                    <button className="scanner_buttons" onClick={() => this.handleClick('insecure_http')}>Responds to HTTP</button>
-                    <button className="scanner_buttons" onClick={() => this.handleClick('redirect_to_https')}>Redirects to HTTPS</button>
-                    <button className="scanner_buttons" onClick={() => this.handleClick('hsts')}>HSTS</button>
-                    <button className="scanner_buttons" onClick={() => this.handleClick('root_ca')}>Root Certificate Authority</button>
-                </div>
-                <input type="text" onChange={this.setDomain}/>
+                <input type="text" value = {this.state.domain} onChange={this.setDomain}/>
+                <MethodsList domain = {this.state.domain}/>
+                {/*<div className="scanner_buttons">*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('ipv4_addresses')}>IPv4</button>*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('ipv6_addresses')}>IPv6</button>*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('rdns_names')}>RDNS</button>*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('rtt_range')}>Round Trip Range</button>*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('tls_versions')}>TLS</button>*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('http_server')}>HTTP Server Name</button>*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('insecure_http')}>Responds to HTTP</button>*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('redirect_to_https')}>Redirects to HTTPS</button>*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('hsts')}>HSTS</button>*/}
+                {/*    <button className="scanner_buttons" onClick={() => this.handleClick('root_ca')}>Root Certificate Authority</button>*/}
+                {/*</div>*/}
+
+                {/*<div className ="output_div_container">*/}
+                {/*    <Output output = {this.state.output} method = {this.state.method}/>*/}
+                {/*</div>*/}
             </div>
         )
     }
 }
+// function MethodsButtons(props){
+class MethodsList extends React.Component{
+    constructor(props) {
+        super(props);
+        console.log("In methods buttons props.domain: " + props.domain);
+    }
+    render() {
+        console.log("in methods buttons render props.domain")
+        return (
+            <div className="scanner_methods_list">
+                <MethodsListItem domain={this.props.domain} method='ipv4_addresses'/>
+                <MethodsListItem domain={this.props.domain} method='ipv6_addresses'/>
+                <MethodsListItem domain={this.props.domain} method='rdns_names'/>
+                <MethodsListItem domain={this.props.domain} method='rtt_range'/>
+                <MethodsListItem domain={this.props.domain} method='tls_versions'/>
+                <MethodsListItem domain={this.props.domain} method='http_server'/>
+                <MethodsListItem domain={this.props.domain} method='insecure_http'/>
+                <MethodsListItem domain={this.props.domain} method='redirect_to_https'/>
+                <MethodsListItem domain={this.props.domain} method='hsts'/>
+                <MethodsListItem domain={this.props.domain} method='root_ca'/>
+            </div>
+        )
+    }
+}
+class MethodsListItem extends React.Component{
+    constructor(props) {
+        super(props);
+        console.log("In methods buttons props.domain: " + props.domain);
+    }
+
+    render() {
+        return (
+            <div className="scanner_method_list_item">
+                <MethodsButton domain={this.props.domain} method ={this.props.method}/>
+            </div>
+        )
+    }
+}
+class MethodsButton extends React.Component{
+    constructor(props) {
+        super(props);
+        this.handleCLick = this.handleClick.bind(this);
+        this.processJson = this.processJson.bind(this);
+        // this.state ={
+        //     domain: this.props['domain'],
+        //     method: this.props['method']
+        // }
+    }
+
+    isValidUrl(inputUrl){
+
+        if(inputUrl.includes(".")) {
+            try {
+                const url = new URL(inputUrl)
+                if (url.protocol === "http:" || url.protocol === "https:") {
+                    return url.hostname;
+                } else{
+                    return false;
+                }
+            } catch (TypeError) {
+                return false;
+            }
+        } else{
+            return false;
+        }
+    }
+    checkDomain(){
+        //TODO add more in depth error checking
+
+        let currDomain = this.props.domain
+        console.log(this.props.domain)
+        if (currDomain === ""){
+            return false;
+        } else if( (currDomain.substring(0,7) === "http://") || currDomain.substring(0,8) === "https://" ){
+            return this.isValidUrl(currDomain);
+        } else {
+            currDomain = "http://" + currDomain;
+            return this.isValidUrl(currDomain);
+        }
+    }
+    handleClick(method){
+        console.log(method);
+        let domainToSend = this.checkDomain();
+        if (domainToSend) {
+            console.log("this.props.domain: " + this.props.domain + " method: " + this.props.method);
+            const requestData = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({"domain": this.props.domain, "method": method})
+
+            }
+            // const response = await fetch()
+            console.log("before Fetch")
+            fetch('http://127.0.0.1:5000/', requestData)
+                // .then(response =>response.json())
+                .then(response => response.json())
+                .then(json => this.processJson(json, method))
+            console.log("afte fetch")
+        } else {
+            alert("invalid url, please enter a valid url");
+        }
+        // console.log(response)
+    }
+
+    processJson(json, method){
+        if (json.output === null){
+            alert("invalid url, please enter a valid url");
+        } else{
+            alert(method + ": " + json.output);
+        }
+    }
+    render(){
+        // let method = this.props.method;
+        return(
+            <div>
+                <button className="scanner_button" onClick={() => this.handleClick(this.props.method)}>{this.props.method}</button>
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
     <ScannerApp/>,
     rootElement
