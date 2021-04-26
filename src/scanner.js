00af2210e7da3c9e27a2111a9757b306c8e3916e
MICAH THOMPKINS <micahthompkins1@gmail.com>
2021-04-26 13:28:14 -0500

    added documentation to scanner and project, also fixed state warning

--- src/scanner.js
+++ src/scanner.js
@@ -1,14 +1,18 @@
 const rootElement = document.getElementById('scanner_react_container');
+
+/**
+ * The component that contains the entire Scanner app. The state to be managed and passed down as props is the current
+ * input in the domain box
+ */
 class ScannerApp extends React.Component{
     constructor(props) {
         super(props);
-        // this.handleCLick = this.handleClick.bind(this);
         this.setDomain = this.setDomain.bind(this);
-        // this.processJson = this.processJson.bind(this);
         this.state ={
             domain: "", method: ""
         }
     }
+    /** method called onchange of the text input to set the state and rerender the component**/
     setDomain(event){
         this.setState({domain: event.target.value}, () => this.render());
     }
@@ -23,6 +27,10 @@ class ScannerApp extends React.Component{
         )
     }
 }
+
+/**
+ * The component that renders the header of the web app doesnt take in props.
+ */
 class HeaderInfo extends React.Component{
     constructor(props) {
         super(props);
@@ -35,7 +43,11 @@ class HeaderInfo extends React.Component{
             </div>
         )
     }
-}// function MethodsButtons(props){
+}
+
+/**
+ * The component that contains all the MethodList items. props.domain is the current domain input
+ */
 class MethodsList extends React.Component{
     constructor(props) {
         super(props);
@@ -59,6 +71,10 @@ class MethodsList extends React.Component{
         )
     }
 }
+
+/**
+ * Component that contains the explanation component and button/output component. props.domain is the current domain input
+ */
 class MethodsListItem extends React.Component{
     constructor(props) {
         super(props);
@@ -74,6 +90,11 @@ class MethodsListItem extends React.Component{
         )
     }
 }
+
+/**
+ * Contains the method explanation. A dictonary of explanations that's keyed on the method names nad uses that dictonary
+ * with the props.method to display the explanation about the method passed in.
+ */
 class MethodsButtonExplanation extends React.Component{
     constructor(props) {
         super(props);
@@ -109,6 +130,9 @@ class MethodsButtonExplanation extends React.Component{
 
 }
 
+/**
+ * The Button and output component. Contains a button labeled by methods shortname keyed to the inputs given by props.method, props.domain is the current domain input
+ */
 class MethodsButton extends React.Component{
     constructor(props) {
         super(props);
@@ -128,16 +152,21 @@ class MethodsButton extends React.Component{
             output: ""
         }
     }
-
+    /**
+     * This method checks to see if the url passed in contains a period and doesnt error when creating a url object. If
+     * both conditions are true it returns the hostname
+     * @param inputUrl: the url to check
+     * @returns {string|boolean}: the hostname if conditions are true otherwise false
+     */
     isValidUrl(inputUrl){
 
         if(inputUrl.includes(".")) {
             try {
-                console.log("inputUrl: " + inputUrl)
+                // console.log("inputUrl: " + inputUrl)
                 const url = new URL(inputUrl)
                 if (url.protocol === "http:" || url.protocol === "https:") {
-                    console.log(url.hostname)
-                    console.log("length: "+ url.hostname.length)
+                    // console.log(url.hostname)
+                    // console.log("length: "+ url.hostname.length)
                     let returnString = url.hostname
                     // return url.hostname;
                     return returnString;
@@ -151,8 +180,14 @@ class MethodsButton extends React.Component{
             return false;
         }
     }
+
+    /** method to check to make sure the inputted domain is an acceptable format
+     * Checks the passed in domain from props.domain, if the domain is empty returns false. if it begins with http:// or
+     * https:// return 2 to tell handleclick it has http, if neither are then prepend http:// and return the value of
+     * isValidUrl
+     * @returns {string|boolean|number}
+     */
     checkDomain(){
-        //TODO add more in depth error checking
 
         let currDomain = this.props.domain
         // console.log(this.props.domain)
@@ -168,6 +203,11 @@ class MethodsButton extends React.Component{
         currDomain = "http://" + currDomain;
         return this.isValidUrl(currDomain);
     }
+
+    /**
+     * Function that checks the domain and then if it is valid sends it to the webserver with the given method
+     * @param method the method that is calling the function
+     */
     handleClick(method){
         // console.log(method);
         if(this.state.output === "") {
@@ -189,7 +229,7 @@ class MethodsButton extends React.Component{
                 fetch('https://scanner-webserver.herokuapp.com/', requestData)
                     // .then(response =>response.json())
                     .then(response => response.json())
-                    .then(json => this.processJson(json, method))
+                    .then(json => this.processJson(json))
                 // console.log("after fetch")
             } else if (domainToSend === 2){
                 this.setState({output: ""}, () => this.render());
@@ -203,7 +243,11 @@ class MethodsButton extends React.Component{
         // console.log(response)
     }
 
-    processJson(json, method){
+    /**
+     * method used to handles the json received from the webserver
+     * @param json the JSON output from the webserver to be processed and set to the output
+     */
+    processJson(json){
         if (json.output === null){
             this.setState({output: ""}, () => this.render());
             alert("Server can't use that url, please enter a different url");
@@ -216,11 +260,26 @@ class MethodsButton extends React.Component{
 
         }
     }
+
+    /**
+     * This changes the output of the methods if the domain is changed
+     * @param props
+     * @param state
+     * @returns {{output: string, domain}|null}
+     */
+    static getDerivedStateFromProps(props, state){
+        if (props.domain !== state.domain){
+            return {domain: props.domain, output: ""};
+        }
+        else{
+            return null;
+        }
+    }
     render(){
         // let method = this.props.method;
-        if (this.state.domain !== this.props.domain){
-            this.setState({domain: this.props.domain, output:""}, ()=>this.render());
-        }
+        // if (this.state.domain !== this.props.domain){
+        //     this.setState({domain: this.props.domain, output:""}, ()=>this.render());
+        // }
         return(
             <div className="method_button_and_output">
                 <button className="scanner_button" onClick={() => this.handleClick(this.props.method)}>{this.method_titles[this.props.method]}</button>
